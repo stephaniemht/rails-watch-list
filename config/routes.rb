4d8957f2989153d1a7f0ffe9224b5232f96f3e39
stephanie mihut <stephanie.mihut@gmail.com>
2024-11-28 16:54:21 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-end
+ resources :lists, exept: [:destroy]
+
+ resources :bookmars, only: :destroy
+ end
